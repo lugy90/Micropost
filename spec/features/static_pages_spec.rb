38dbfdd2c07459a548e419300eae35b394cadd73
lugy90 <naitianzong@outlook.com>
2015-05-02 17:22:23 +0800

    complete pure static pages

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+  end 
+  #end of Home_pages
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+  #end of Help_pages
+
+  describe "About page" do
+
+  	it "should have the content 'About Us'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About Us')
+  	end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end 
+  #end of About_pages
+end 
+#end of static_pages
