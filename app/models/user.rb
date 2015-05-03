d7f31ae36402a5da940d0bed5cced1c97faf5d73
lugy90 <naitianzong@outlook.com>
2015-05-03 16:23:22 +0800

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	# These validation just effect in memory not in database( before data saving in the database )
+	validates :name,  presence: true, length: { maximum: 50 }
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
+	validates :email, 
+	          presence: true, 
+	          format: { with: VALID_EMAIL_REGEX }, 
+	          uniqueness: { case_sensitive: false } # Same chars diff case can`t go through
+    
+    has_secure_password                                        
+end
