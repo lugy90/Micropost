45093f34c3a2529787e42285df19800e8e323023
lugy90 <naitianzong@outlook.com>
2015-05-11 15:37:21 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+
+    has_many :microposts, dependent: :destroy
+
 	before_save { self.email = email.downcase }
-	# These validation just effect in memory not in database( before data saving in the database )
+	
 	validates :name,  presence: true, length: { maximum: 50 }
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,6 +24,11 @@ class User < ActiveRecord::Base
 
     def User.hash(token)
         Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    def feed
+      # This is preliminary. See "Following users" for the full implementation.
+      Micropost.where("user_id = ?", id)
     end
 
     private
